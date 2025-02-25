a53f3712cc910108614fe15b5ebd4fbefca20047
Gonzalo <gonzaloalbertoperezcabrera@alumno.ieselrincon.es>
2025-02-25 13:26:45 +0000

    Inicio

---
+++ src/components/Inicio.tsx
@@ -0,0 +1,32 @@
+import '../assets/DL_1.jpeg'
+import '../assets/DL_2.jpeg'
+import '../assets/DL_3.jpeg'
+import '../assets/DL_4.jpeg'
+import '../assets/DL_5.jpeg'
+import '../assets/DL_6.jpeg'
+import '../assets/DuaLab.jpeg'
+
+import '../styles/Inicio.css'
+
+const Inicio = () => {
+    return (
+        <div>
+
+            <div className="encabezado">
+                <img src='../src/assets/DuaLab.jpeg'/>
+            </div>
+
+            <div className="container">
+                <img src='../src/assets/DL_1.jpeg'/>
+                <img src='../src/assets/DL_2.jpeg'/>
+                <img src='../src/assets/DL_3.jpeg'/>
+                <img src='../src/assets/DL_4.jpeg'/>
+                <img src='../src/assets/DL_5.jpeg'/>
+                <img src='../src/assets/DL_6.jpeg'/>
+        </div>
+
+        </div>
+    );
+};
+
+export default Inicio;
